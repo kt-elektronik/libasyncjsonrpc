3cb79ee67e51baab5594b71940b94c9d2f413ed8
Dirk Kaar <kaar@kt-elektronik.de>
2023-05-11 13:13:05 +0200

    EventGenerator ConfigureAwait and Dispose patterns

--- AsyncRPCCore/EventGenerator.cs
+++ AsyncRPCCore/EventGenerator.cs
@@ -1,24 +1,29 @@
 ﻿namespace AsyncRPCCore
 {
-    public sealed record class EventGenerator<T>(CancellationToken Token = default)
+    public sealed record class EventGenerator<T>(CancellationToken Token = default) : IDisposable
     {
         private TaskCompletionSource<T> tcs = new();
         private readonly SemaphoreSlim next = new(1, 1);
 
         public async void OnEvent(object? _, T e)
         {
-            await next.WaitAsync(Token);
+            await next.WaitAsync(Token).ConfigureAwait(false);
             tcs.SetResult(e);
         }
         public async IAsyncEnumerable<T> OnEventAsync()
         {
             while (!Token.IsCancellationRequested)
             {
-                var value = await tcs.Task;
+                var value = await tcs.Task.ConfigureAwait(false);
                 tcs = new TaskCompletionSource<T> { };
                 next.Release();
                 yield return value;
             }
+        }
+
+        public void Dispose()
+        {
+            next.Dispose();
         }
     }
 }
